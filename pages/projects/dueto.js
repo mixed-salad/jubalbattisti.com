6cd7dfb82fc7dcb48218b8416b1886b2ef2f1f53
Harumi Terayama <hterayama@gmail.com>
2021-06-13 23:39:54 +0200

    project index pictures replaced. added DUETO project

---
+++ pages/projects/dueto.js
@@ -0,0 +1,84 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { getList } from "../api/cloudinary";
+import styles from "./../../styles/p4.module.scss";
+import { motion } from "framer-motion";
+
+let easing = [0.175, 0.85, 0.42, 0.96];
+const imgVariants = {
+  initial: { opacity: 0, scale: 0.9 },
+  enter: { opacity: 1, scale: 1, transition: { duration: 1, ease: easing } },
+  exit: { opacity: 0, scale: 2, transition: { duration: 0.5, ease: easing } },
+};
+
+const Dueto = () => {
+  const [photoList, setPhotoList] = useState([]);
+  const [sliderCount, setSliderCount] = useState(0);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      const list = await getList("dueto");
+      console.log(list);
+      setPhotoList(list);
+    };
+    fetchList();
+  }, []);
+
+  const decrementSliderCount = () => {
+    if (sliderCount > 0) {
+      setSliderCount((prev) => prev - 1);
+    } else {
+      setSliderCount(photoList.length - 1);
+    }
+  };
+
+  const incrementSliderCount = () => {
+    if (sliderCount < photoList.length - 1) {
+      setSliderCount((prev) => prev + 1);
+    } else {
+      setSliderCount(0);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Jubal Battisti Photography | DUETO</title>
+        <meta name="keywords" content="photography/videography" />
+      </Head>
+      <div className="main">
+        <h1>DUETO</h1>
+        {!!photoList.length && (
+          <div className={styles.slider}>
+            <div className={styles.imgWrapper}>
+              <motion.img variants={imgVariants}>
+                <Image
+                  src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photoList[sliderCount].public_id}`}
+                  width={400}
+                  height={500}
+                  leyout="fill"
+                />
+              </motion.img>
+            </div>
+            <div className={styles.sliderNavigationGroup}>
+              <div
+                onClick={decrementSliderCount}
+                className={styles.sliderNavigationLeft}
+              ></div>
+              <div
+                onClick={incrementSliderCount}
+                className={styles.sliderNavigationRight}
+              ></div>
+            </div>
+            <p className={styles.caption}>
+              {photoList[sliderCount].context.custom.caption}
+            </p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Dueto;
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -11,13 +11,30 @@ const variants = {
   exit: { opacity: 0, scale: 2, transition: { duration: 1, ease: easing } },
 };
 
+// const hoverVariants = {
+//   scale: 0.98,
+//   transition: { duration: 0.5, ease: easing },
+// };
+
 const Projects = () => {
+  const handleMouseOver = (project) => {
+    const target = document.getElementById(project);
+    target.classList.add(styles.hover);
+  };
+  const handleMouseLeave = (project) => {
+    console.log(project, "handling mouse leave");
+
+    const target = document.getElementById(project);
+    target.classList.remove(styles.hover);
+  };
+
   return (
     <>
       <Head>
         <title>Jubal Battisti Photography | Projects</title>
         <meta name="keywords" content="photography/videography" />
       </Head>
+      {/* Framer motion transitions */}
       <motion.div
         initial="initial"
         animate="enter"
@@ -27,27 +44,44 @@ const Projects = () => {
         <div className="main">
           <div className={styles.projectsWrapper}>
             <Link href="/projects/p4">
-              <div className={styles.project}>
-                <Image publicId="Jubal_battisti/p4_1_fkaxax.jpg" />
+              <div
+                id="p4"
+                className={styles.project}
+                onMouseOver={() => handleMouseOver("p4")}
+                onMouseLeave={() => handleMouseLeave("p4")}
+              >
+                <Image publicId="Jubal_battisti/p4/190409_P_4_Coming_Society_by_Jubal_Battisti_114-Edit_copy_hzeued.jpg" />
                 <h3 className={styles.projectTitle}>P4</h3>
               </div>
             </Link>
             <Link href="/projects/works-in-process">
-              <div className={styles.project}>
+              <div id="p4" className={styles.project}>
                 <Image publicId="Jubal_battisti/process/process_2_j3tipo.jpg" />
-                <h3 className={styles.projectTitle}>Works in Process</h3>
+                <h3 className={styles.projectTitle}>
+                  Works
+                  <br />
+                  in
+                  <br />
+                  Process
+                </h3>
               </div>
             </Link>
             <Link href="/projects/performance">
-              <div className={styles.project}>
+              <div id="p4" className={styles.project}>
                 <Image publicId="Jubal_battisti/performance/190212_Staatsballett_Berlin_Distant_Matter_by_Jubal_Battisti_0099_nj8lqj.jpg" />
-                <h3 className={styles.projectTitle}>Performace</h3>
+                <h3 className={styles.projectTitle}>Performance</h3>
               </div>
             </Link>
             <Link href="/projects/moving">
-              <div className={styles.project}>
+              <div id="p4" className={styles.project}>
                 <Image publicId="Jubal_battisti/performance/190212_Staatsballett_Berlin_Distant_Matter_by_Jubal_Battisti_1594_ul1upp.jpg" />
                 <h3 className={styles.projectTitle}>Moving</h3>
+              </div>
+            </Link>
+            <Link href="/projects/dueto">
+              <div id="p4" className={styles.project}>
+                <Image publicId="Jubal_battisti/dueto/DUETO_Malika-133-Edit_by_Jubal_Battisti_mygtiu.jpg" />
+                <h3 className={styles.projectTitle}>DUETO</h3>
               </div>
             </Link>
           </div>
