3813a448a85a68518da32c01a28f5e1f7f56a89e
Harumi Terayama <hterayama@gmail.com>
2021-06-22 21:41:06 +0200

    nav bar expantion effect

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,35 +1,114 @@
 import Link from "next/link";
 import Image from "next/image";
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaCompressAlt } from "react-icons/fa";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
+const navVariants = {
+  initial: {
+    opacity: 0,
+    zIndex: 3,
+    transition: {
+      duration: 1,
+    },
+  },
+  complete: {
+    opacity: 1,
+
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const linkWrapperVariants = {
+  intitial: {
+    opacity: 0,
+  },
+  complete: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const linkVariants = {
+  initial: {
+    x: 30,
+    opacity: 0,
+  },
+  complete: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
 const Navbar = () => {
+  const [navView, setNavView] = useState(false);
+
   return (
-    <nav>
-      <div className="logo">
-        <Link href="/">
-          <a>
-            <Image src="/logos/logo.png" width={110} height={65} />
-          </a>
-        </Link>
+    <>
+      <div className="header-section">
+        <div className="logo">
+          <Link href="/">
+            <a>
+              <Image src="/logos/logo.png" width={110} height={65} />
+            </a>
+          </Link>
+        </div>
+        <div className="navClosedIcon" onClick={() => setNavView(!navView)}>
+          <span className="line">➖</span>
+          <span className="line">➖</span>
+          <span className="line">➖</span>
+        </div>
       </div>
-      <div className="navLinks">
-        <Link href="/projects">
-          <a>Projects</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/contact">
-          <a>Contact</a>
-        </Link>
-        <a
-          href="https://www.instagram.com/jubalbattistiphotography/"
-          target="_blank"
-        >
-          <FaInstagram />
-        </a>
-      </div>
-    </nav>
+      {navView && (
+        <AnimatePresence exitBeforeEnter>
+          <motion.nav
+            initial="initial"
+            animate="complete"
+            exit="initial"
+            variants={navVariants}
+            className="nav-section"
+          >
+            <motion.div
+              className="close-nav-icon"
+              onClick={() => setNavView(!navView)}
+            >
+              <FaCompressAlt />
+            </motion.div>
+            <motion.div className="navLinks" variants={linkWrapperVariants}>
+              <motion.div className="navLinks__link" variants={linkVariants}>
+                <Link href="/projects">
+                  <a>Projects</a>
+                </Link>
+              </motion.div>
+              <motion.div className="navLinks__link" variants={linkVariants}>
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </motion.div>
+              <motion.div className="navLinks__link" variants={linkVariants}>
+                <Link href="/contact">
+                  <a>Contact</a>
+                </Link>
+              </motion.div>
+
+              <motion.div className="navLinks__link" variants={linkVariants}>
+                <a
+                  href="https://www.instagram.com/jubalbattistiphotography/"
+                  target="_blank"
+                >
+                  <FaInstagram />
+                </a>
+              </motion.div>
+            </motion.div>
+          </motion.nav>
+        </AnimatePresence>
+      )}
+    </>
   );
 };
 
