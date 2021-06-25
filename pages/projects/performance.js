92a866cf88132effb91d7f2af072cd9afbf81836
Harumi Terayama <hterayama@gmail.com>
2021-06-25 16:03:29 +0200

    refactor performance page. needs styling

--- pages/projects/performance.js
+++ pages/projects/performance.js
@@ -1,44 +1,67 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import { getList } from "../api/cloudinary";
-import SinglePerformance from "./../../components/SinglePerformance";
+import SinglePhoto from "../../components/SinglePhoto";
+import { motion } from "framer-motion";
+import styles from "./../../styles/performance.module.scss"
+
+const mainVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transform: {
+      duration: .5,
+      staggerChildren: .2
+    }
+  }
+}
+
+const imgVariants = {
+  initial: {
+    opacity: 0,
+    y: 20
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transform: {
+      delay: .2,
+      duration: .5,
+      staggerChildren: .2
+    }
+  }
+}
 
 const Performance = () => {
   const [photoList, setPhotoList] = useState([]);
-  const [companies, setCompanies] = useState([]);
-
+  
   useEffect(() => {
     const fetchList = async () => {
       const list = await getList("performance");
       setPhotoList(list);
-      const companiesList = [];
-      for (let imgObject of list) {
-        if (!companiesList.includes(imgObject.context.custom.company)) {
-          companiesList.push(imgObject.context.custom.company);
-        }
-      }
-      // console.log(companiesList);
-      setCompanies(companiesList);
     };
     fetchList();
   }, []);
-
+  
   return (
     <>
       <Head>
-        <title>Jubal Battisti Photography | Performances</title>
+        <title>Jubal Battisti Photography | Performance</title>
         <meta name="keywords" content="photography/videography" />
       </Head>
       <div className="main">
-        <h1>Performance</h1>
-        {!!companies.length &&
-          companies.map((company) => (
-            <SinglePerformance
-              key={company}
-              company={company}
-              photoList={photoList}
-            />
+        <motion.div className={styles.photoGalleryWrapper} initial="initial" animate="animate" variants={mainVariants}>
+
+        {!!photoList.length &&
+          photoList.map((photo) => (
+            <motion.div key={photo.public_id} className={styles.photoWrapper} variants={imgVariants}>
+            <motion.img src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photo.public_id}`}/>
+            </motion.div>
           ))}
+          </motion.div>
       </div>
     </>
   );
