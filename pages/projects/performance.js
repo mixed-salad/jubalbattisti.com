9d56984fce2858ddd9a6750a0ba96b2b6cabe2ee
Harumi Terayama <hterayama@gmail.com>
2021-06-26 18:02:32 +0200

    performance page lightbox basic structure is there

--- pages/projects/performance.js
+++ pages/projects/performance.js
@@ -4,8 +4,9 @@ import { getList } from "../api/cloudinary";
 import SinglePhoto from "../../components/SinglePhoto";
 import { motion } from "framer-motion";
 import styles from "./../../styles/performance.module.scss"
+import { FaTimesCircle } from "react-icons/fa"
 
-const mainVariants = {
+const galleryVariants = {
   initial: {
     opacity: 0,
   },
@@ -37,6 +38,8 @@ const imgVariants = {
 
 const Performance = () => {
   const [photoList, setPhotoList] = useState([]);
+  const [lightboxVisible, setLightboxVisible] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(1);
   
   useEffect(() => {
     const fetchList = async () => {
@@ -45,6 +48,22 @@ const Performance = () => {
     };
     fetchList();
   }, []);
+
+  const openLightbox = (index) => {
+    setLightboxVisible(true);
+    setPhotoIndex(index);
+  }
+
+  const closeLightbox = () => setLightboxVisible(false);
+
+  const decreasePhotoIndex = () => {
+    photoIndex < 2 ? setPhotoIndex(photoList.length-1) : setPhotoIndex(photoIndex - 1);
+  }
+  const increasePhotoIndex = () => {
+    photoIndex > photoList.length - 2 ? setPhotoIndex(1) : setPhotoIndex(photoIndex + 1);
+  }
+
+  console.log(photoIndex);
   return (
     <>
       <Head>
@@ -52,19 +71,36 @@ const Performance = () => {
         <meta name="keywords" content="photography/videography" />
       </Head>
       <div className="main">
-        <motion.div className={styles.photoGalleryWrapper} initial="initial" animate="animate" variants={mainVariants}>
+        {/* This is the to show the gallery */}
+        <motion.div className={styles.photoGalleryWrapper} initial="initial" animate="animate" variants={galleryVariants}>
           {!!photoList.length &&
             photoList.map((photo, i) => {
               if(i === 0) {
                 return (<div className={`${styles["card"]} ${styles["title"]}`}><h2>performance</h2></div>)
               } else {
-                return (<motion.div key={photo.public_id} className={styles.card} variants={imgVariants}>
+                return (<motion.div key={photo.public_id} className={styles.card} variants={imgVariants} onClick={() => openLightbox(i)}>
                 <motion.img className={styles.img} src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photo.public_id}`}/>
                 </motion.div>)
               }
             })
           }
           </motion.div>
+          {/* This is to show the lightbox when the photo is clicked */}
+          {lightboxVisible && (
+            <motion.div className={styles.modal}>
+              <span onClick={() => closeLightbox()} className={styles.closeLightbox}><FaTimesCircle size="2em"/></span>
+              <motion.div className={styles.modalContent}>
+                <motion.img className={styles.lightboxImg} src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photoList[photoIndex].public_id}`}></motion.img>
+              </motion.div>
+              <div className={styles.sliderNavigationGroup}>
+                <div className={styles.sliderNavigationLeft} onClick={() => decreasePhotoIndex()}></div>
+                <div className={styles.sliderNavigationRight} onClick={() => increasePhotoIndex()}></div>
+              </div>
+              <div>
+                <span>{photoIndex} / {photoIndex.length - 1}</span>
+              </div>
+            </motion.div>
+          )}
       </div>
     </>
   );
