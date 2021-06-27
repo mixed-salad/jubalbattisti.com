3a46beb48b12696531174db64030e1ea3cd2b42a
Harumi Terayama <hterayama@gmail.com>
2021-06-27 15:52:04 +0200

    perfomance page: lightbox, change the color of the arrows and close button

--- pages/projects/performance.js
+++ pages/projects/performance.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import { getList } from "../api/cloudinary";
 import SinglePhoto from "../../components/SinglePhoto";
 import { motion } from "framer-motion";
-import styles from "./../../styles/performance.module.scss"
-import { FaTimesCircle } from "react-icons/fa"
+import styles from "./../../styles/performance.module.scss";
+import { FaTimesCircle } from "react-icons/fa";
 
 const galleryVariants = {
   initial: {
@@ -14,33 +14,33 @@ const galleryVariants = {
     opacity: 1,
     y: 0,
     transform: {
-      duration: .5,
-      staggerChildren: .2
-    }
-  }
-}
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const imgVariants = {
   initial: {
     opacity: 0,
-    y: 20
+    y: 20,
   },
   animate: {
     opacity: 1,
     y: 0,
     transform: {
-      delay: .2,
-      duration: .5,
-      staggerChildren: .2
-    }
-  }
-}
+      delay: 0.2,
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const Performance = () => {
   const [photoList, setPhotoList] = useState([]);
   const [lightboxVisible, setLightboxVisible] = useState(false);
   const [photoIndex, setPhotoIndex] = useState(1);
-  
+
   useEffect(() => {
     const fetchList = async () => {
       const list = await getList("performance");
@@ -52,16 +52,20 @@ const Performance = () => {
   const openLightbox = (index) => {
     setLightboxVisible(true);
     setPhotoIndex(index);
-  }
+  };
 
   const closeLightbox = () => setLightboxVisible(false);
 
   const decreasePhotoIndex = () => {
-    photoIndex < 2 ? setPhotoIndex(photoList.length-1) : setPhotoIndex(photoIndex - 1);
-  }
+    photoIndex < 2
+      ? setPhotoIndex(photoList.length - 1)
+      : setPhotoIndex(photoIndex - 1);
+  };
   const increasePhotoIndex = () => {
-    photoIndex > photoList.length - 2 ? setPhotoIndex(1) : setPhotoIndex(photoIndex + 1);
-  }
+    photoIndex > photoList.length - 2
+      ? setPhotoIndex(1)
+      : setPhotoIndex(photoIndex + 1);
+  };
 
   console.log(photoIndex);
   return (
@@ -72,35 +76,69 @@ const Performance = () => {
       </Head>
       <div className="main">
         {/* This is the to show the gallery */}
-        <motion.div className={styles.photoGalleryWrapper} initial="initial" animate="animate" variants={galleryVariants}>
+        <motion.div
+          className={styles.photoGalleryWrapper}
+          initial="initial"
+          animate="animate"
+          variants={galleryVariants}
+        >
           {!!photoList.length &&
             photoList.map((photo, i) => {
-              if(i === 0) {
-                return (<div className={`${styles["card"]} ${styles["title"]}`}><h2>performance</h2></div>)
+              if (i === 0) {
+                return (
+                  <div className={`${styles["card"]} ${styles["title"]}`}>
+                    <h2>performance</h2>
+                  </div>
+                );
               } else {
-                return (<motion.div key={photo.public_id} className={styles.card} variants={imgVariants} onClick={() => openLightbox(i)}>
-                <motion.img className={styles.img} src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photo.public_id}`}/>
-                </motion.div>)
+                return (
+                  <motion.div
+                    key={photo.public_id}
+                    className={styles.card}
+                    variants={imgVariants}
+                    onClick={() => openLightbox(i)}
+                  >
+                    <motion.img
+                      className={styles.img}
+                      src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photo.public_id}`}
+                    />
+                  </motion.div>
+                );
               }
-            })
-          }
-          </motion.div>
-          {/* This is to show the lightbox when the photo is clicked */}
-          {lightboxVisible && (
-            <motion.div className={styles.modal}>
-              <span onClick={() => closeLightbox()} className={styles.closeLightbox}><FaTimesCircle size="2em"/></span>
-              <motion.div className={styles.modalContent}>
-                <motion.img className={styles.lightboxImg} src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photoList[photoIndex].public_id}`}></motion.img>
-              </motion.div>
-              <div className={styles.sliderNavigationGroup}>
-                <div className={styles.sliderNavigationLeft} onClick={() => decreasePhotoIndex()}></div>
-                <div className={styles.sliderNavigationRight} onClick={() => increasePhotoIndex()}></div>
-              </div>
-              <div>
-                <span>{photoIndex} / {photoList.length - 1}</span>
-              </div>
+            })}
+        </motion.div>
+        {/* This is to show the lightbox when the photo is clicked */}
+        {lightboxVisible && (
+          <motion.div className={styles.modal}>
+            <span
+              onClick={() => closeLightbox()}
+              className={styles.closeLightbox}
+            >
+              <FaTimesCircle size="2em" />
+            </span>
+            <motion.div className={styles.modalContent}>
+              <motion.img
+                className={styles.lightboxImg}
+                src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photoList[photoIndex].public_id}`}
+              ></motion.img>
             </motion.div>
-          )}
+            <div className={styles.sliderNavigationGroup}>
+              <div
+                className={styles.sliderNavigationLeft}
+                onClick={() => decreasePhotoIndex()}
+              ></div>
+              <div
+                className={styles.sliderNavigationRight}
+                onClick={() => increasePhotoIndex()}
+              ></div>
+            </div>
+            <div>
+              <span>
+                {photoIndex} / {photoList.length - 1}
+              </span>
+            </div>
+          </motion.div>
+        )}
       </div>
     </>
   );
