a007ac7d01f23f518b03ecadf967423c5ca82bf5
Harumi Terayama <hterayama@gmail.com>
2021-07-01 23:19:42 +0200

    separated navBar depending on the mobile or desktop. needs styling change for desktop version

---
+++ components/DesktopNav.jsx
@@ -0,0 +1,102 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FaInstagram, FaCompressAlt } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
+
+const navVariants = {
+  initial: {
+    opacity: 0,
+    zIndex: 3,
+    transition: {
+      duration: 1,
+    },
+  },
+  complete: {
+    opacity: 1,
+
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const linkWrapperVariants = {
+  intitial: {
+    opacity: 0,
+  },
+  complete: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const linkVariants = {
+  initial: {
+    x: 30,
+    opacity: 0,
+  },
+  complete: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+const DesktopNav = () => {
+  return (
+    <>
+      <AnimatePresence exitBeforeEnter>
+        <motion.nav
+          initial="initial"
+          animate="complete"
+          exit="initial"
+          variants={navVariants}
+          className="nav-section"
+        >
+          <motion.div className="navLinks" variants={linkWrapperVariants}>
+            <motion.div className="navLinks__link" variants={linkVariants}>
+              <Link href="/projects">
+                <a>Projects</a>
+              </Link>
+            </motion.div>
+            <motion.div
+              className="navLinks__link"
+              variants={linkVariants}
+              onClick={() => setNavView(!navView)}
+            >
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </motion.div>
+            <motion.div
+              className="navLinks__link"
+              variants={linkVariants}
+              onClick={() => setNavView(!navView)}
+            >
+              <Link href="/contact">
+                <a>Contact</a>
+              </Link>
+            </motion.div>
+
+            <motion.div
+              className="navLinks__link"
+              variants={linkVariants}
+              onClick={() => setNavView(!navView)}
+            >
+              <a
+                href="https://www.instagram.com/jubalbattistiphotography/"
+                target="_blank"
+              >
+                <FaInstagram />
+              </a>
+            </motion.div>
+          </motion.div>
+        </motion.nav>
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default DesktopNav;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,48 +3,9 @@ import Image from "next/image";
 import { FaInstagram, FaCompressAlt } from "react-icons/fa";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
+import MobileNav from "./MobileNav";
+import DesktopNav from "./DesktopNav";
 
-const navVariants = {
-  initial: {
-    opacity: 0,
-    zIndex: 3,
-    transition: {
-      duration: 1,
-    },
-  },
-  complete: {
-    opacity: 1,
-
-    transition: {
-      duration: 1,
-    },
-  },
-};
-
-const linkWrapperVariants = {
-  intitial: {
-    opacity: 0,
-  },
-  complete: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const linkVariants = {
-  initial: {
-    x: 30,
-    opacity: 0,
-  },
-  complete: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.4,
-    },
-  },
-};
 const Navbar = () => {
   const [navView, setNavView] = useState(false);
 
@@ -58,68 +19,13 @@ const Navbar = () => {
             </a>
           </Link>
         </div>
-        <div className="navClosedIcon" onClick={() => setNavView(!navView)}>
-          <span className="line">➖</span>
-          <span className="line">➖</span>
-          <span className="line">➖</span>
+        <div className="display-xs">
+          <MobileNav />
+        </div>
+        <div className="display-md">
+          <DesktopNav />
         </div>
       </div>
-      {navView && (
-        <AnimatePresence exitBeforeEnter>
-          <motion.nav
-            initial="initial"
-            animate="complete"
-            exit="initial"
-            variants={navVariants}
-            className="nav-section"
-          >
-            <motion.div
-              className="close-nav-icon"
-              onClick={() => setNavView(!navView)}
-            >
-              <FaCompressAlt />
-            </motion.div>
-            <motion.div className="navLinks" variants={linkWrapperVariants}>
-              <motion.div className="navLinks__link" variants={linkVariants}>
-                <Link href="/projects">
-                  <a>Projects</a>
-                </Link>
-              </motion.div>
-              <motion.div
-                className="navLinks__link"
-                variants={linkVariants}
-                onClick={() => setNavView(!navView)}
-              >
-                <Link href="/about">
-                  <a>About</a>
-                </Link>
-              </motion.div>
-              <motion.div
-                className="navLinks__link"
-                variants={linkVariants}
-                onClick={() => setNavView(!navView)}
-              >
-                <Link href="/contact">
-                  <a>Contact</a>
-                </Link>
-              </motion.div>
-
-              <motion.div
-                className="navLinks__link"
-                variants={linkVariants}
-                onClick={() => setNavView(!navView)}
-              >
-                <a
-                  href="https://www.instagram.com/jubalbattistiphotography/"
-                  target="_blank"
-                >
-                  <FaInstagram />
-                </a>
-              </motion.div>
-            </motion.div>
-          </motion.nav>
-        </AnimatePresence>
-      )}
     </>
   );
 };
