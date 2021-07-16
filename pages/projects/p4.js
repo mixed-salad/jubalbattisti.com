c13822d396fa5447a4595f3f1320c28bf1833f13
Harumi Terayama <hterayama@gmail.com>
2021-07-16 12:28:40 +0300

    P4 mobile page: centered the image and the text

--- pages/projects/p4.js
+++ pages/projects/p4.js
@@ -1,22 +1,22 @@
-import Head from "next/head";
-import Image from "next/image";
-import { useState, useEffect } from "react";
-import { getList } from "../api/cloudinary";
-import styles from "./../../styles/p4.module.scss";
-import { motion } from "framer-motion";
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { getList } from '../api/cloudinary';
+import styles from './../../styles/p4.module.scss';
+import { motion } from 'framer-motion';
 
 const imgVariants = {
   initial: {
     opacity: 0,
-    x: 20,
+    x: 20
   },
   complete: {
     opacity: 1,
     x: 0,
     transition: {
-      duration: 0.5,
-    },
-  },
+      duration: 0.5
+    }
+  }
 };
 const P4 = () => {
   const [photoList, setPhotoList] = useState([]);
@@ -24,7 +24,7 @@ const P4 = () => {
 
   useEffect(() => {
     const fetchList = async () => {
-      const list = await getList("p4");
+      const list = await getList('p4');
       console.log(list);
       setPhotoList(list);
     };
@@ -57,38 +57,47 @@ const P4 = () => {
         <main className={styles.p4Main}>
           <section className={styles.photoSection}>
             {!!photoList.length && (
-            <div className={styles.slider}>
-              <div className={styles.imgWrapper}>
-                <motion.img
-                  src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photoList[sliderCount].public_id}`}
-                  className={styles.p4_img}
-                  initial="initial"
-                  animate="complete"
-                  variants={imgVariants}
-                />
+              <div className={styles.slider}>
+                <div className={styles.imgWrapper}>
+                  <motion.img
+                    src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${photoList[sliderCount].public_id}`}
+                    className={styles.p4_img}
+                    initial="initial"
+                    animate="complete"
+                    variants={imgVariants}
+                  />
+                </div>
+                <div className={styles.sliderNavigationGroup}>
+                  <div
+                    onClick={decrementSliderCount}
+                    className={styles.sliderNavigationLeft}
+                  ></div>
+                  <div
+                    onClick={incrementSliderCount}
+                    className={styles.sliderNavigationRight}
+                  ></div>
+                </div>
               </div>
-              <div className={styles.sliderNavigationGroup}>
-                <div
-                  onClick={decrementSliderCount}
-                  className={styles.sliderNavigationLeft}
-                ></div>
-                <div
-                  onClick={incrementSliderCount}
-                  className={styles.sliderNavigationRight}
-                ></div>
-              </div>
-            </div>
-          )}
-        </section>
-        <section className={styles.description}>
-          <p>
-            <em>
-              <strong>Post Performace Portrait Project (P^4)</strong>
-            </em>{" "}
-            captures performing artists at the moment of transition when they
-            are just off the stage....{" "}
-          </p>
-        </section>
+            )}
+          </section>
+          <section className={styles.description}>
+            <p>
+              <em>
+                <strong>Post Performace Portrait Project (P^4)</strong>
+              </em>
+            </p>
+            <p>
+              What lies beneath the ambition to perform? What remains
+              afterwards?{' '}
+            </p>
+            <p>
+              As a performer intrigued by these questions, I began making
+              portraits with my fellow performers immediately after we stepped
+              offstage. Since 2009, the Post-Performance Portrait Project has
+              recorded more than one hundred performers from various dance
+              companies as well as operas and musicals.
+            </p>
+          </section>
         </main>
       </div>
     </>
