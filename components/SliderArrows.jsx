567e513dbb9325ad1e821a25ce4acb52dca6b910
Harumi Terayama <hterayama@gmail.com>
2021-07-28 16:04:55 +0200

    P4 and DUETO page: color of the arrows are now #969696

--- components/SliderArrows.jsx
+++ components/SliderArrows.jsx
@@ -2,15 +2,8 @@ import styles from "./../styles/p4.module.scss";
 
 const SliderArrow = (props) => {
   let className = props.type === "next" ? "nextArrow" : "prevArrow";
-  const charSrc =
-    props.type === "next"
-      ? "/images/chevron-right_white.png"
-      : "/images/chevron-left_white.png";
-  return (
-    <div className={styles[className]} onClick={props.onClick}>
-      {/* <img className={styles.arrows} src={charSrc} alt="arrow" /> */}
-    </div>
-  );
+
+  return <div className={styles[className]} onClick={props.onClick}></div>;
 };
 
 export default SliderArrow;
