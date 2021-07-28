40be530f766348546d26b597510221821cc74f55
Harumi Terayama <hterayama@gmail.com>
2021-07-28 15:24:21 +0200

    P4 page: The arrows are now displayed by the cursor and not as buttons

--- components/SliderArrows.jsx
+++ components/SliderArrows.jsx
@@ -1,14 +1,14 @@
-import styles from './../styles/p4.module.scss';
+import styles from "./../styles/p4.module.scss";
 
 const SliderArrow = (props) => {
-  let className = props.type === 'next' ? 'nextArrow' : 'prevArrow';
+  let className = props.type === "next" ? "nextArrow" : "prevArrow";
   const charSrc =
-    props.type === 'next'
-      ? '/images/chevron-right_white.png'
-      : '/images/chevron-left_white.png';
+    props.type === "next"
+      ? "/images/chevron-right_white.png"
+      : "/images/chevron-left_white.png";
   return (
     <div className={styles[className]} onClick={props.onClick}>
-      <img className={styles.arrows} src={charSrc} alt="arrow" />
+      {/* <img className={styles.arrows} src={charSrc} alt="arrow" /> */}
     </div>
   );
 };
