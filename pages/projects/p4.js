021305ccea5e8393f072e22b282a3df4356a7ec5
Harumi Terayama <hterayama@gmail.com>
2021-07-28 16:18:15 +0200

    DUETO and P4: added padding between the images in the slider

--- pages/projects/p4.js
+++ pages/projects/p4.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import styles from './../../styles/p4.module.scss';
-import Carousel from '../../components/Carousel';
+import Head from "next/head";
+import styles from "./../../styles/p4.module.scss";
+import Carousel from "../../components/Carousel";
 
 const P4 = () => {
   return (
@@ -19,12 +19,14 @@ const P4 = () => {
             </p>
             <p>
               What lies beneath the ambition to perform? What remains
-              afterwards?{' '}
+              afterwards?{" "}
             </p>
             <p>
               As a performer intrigued by these questions, I began making
               portraits with my fellow performers immediately after we stepped
-              offstage. <br />
+              offstage.
+            </p>
+            <p>
               Since 2009, the Post-Performance Portrait Project has recorded
               more than one hundred performers from various dance companies as
               well as operas and musicals.
