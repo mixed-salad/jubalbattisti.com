13f88d0826bc703e9a07777eda858cb3edd63ead
Harumi Terayama <hterayama@gmail.com>
2021-08-01 19:45:29 +0200

    project index page: got rid of the flex-basis from the project class

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -55,7 +55,7 @@ const Projects = () => {
         >
           <motion.div className={styles.project} variants={itemVariants}>
             <Link href="/projects/moving">
-              <div id="moving">
+              <div className={styles.projectContainer} id="moving">
                 <img src="/Thumnails/moving_thumbnail.png" />
                 <h3 className={styles.projectTitle}>motion</h3>
               </div>
@@ -63,7 +63,7 @@ const Projects = () => {
           </motion.div>
           <motion.div className={styles.project} variants={itemVariants}>
             <Link href="/projects/p4">
-              <div id="p4">
+              <div id="p4" className={styles.projectContainer}>
                 <Image publicId="Jubal_battisti/p4/190409_P_4_Coming_Society_by_Jubal_Battisti_114-Edit_copy_hzeued.jpg" />
                 <h3 className={styles.projectTitle}>
                   P^4
@@ -83,7 +83,7 @@ const Projects = () => {
           </motion.div>
           <motion.div className={styles.project} variants={itemVariants}>
             <Link href="/projects/works-in-process">
-              <div id="process">
+              <div id="process" className={styles.projectContainer}>
                 <Image publicId="Jubal_battisti/process/process_2_j3tipo.jpg" />
                 <h3 className={styles.projectTitle}>works in process</h3>
               </div>
@@ -91,7 +91,7 @@ const Projects = () => {
           </motion.div>
           <motion.div className={styles.project} variants={itemVariants}>
             <Link href="/projects/performance">
-              <div id="performance">
+              <div id="performance" className={styles.projectContainer}>
                 <Image publicId="Jubal_battisti/performance/190212_Staatsballett_Berlin_Distant_Matter_by_Jubal_Battisti_0099_nj8lqj.jpg" />
                 <h3 className={styles.projectTitle}>performance</h3>
               </div>
@@ -99,7 +99,7 @@ const Projects = () => {
           </motion.div>
           <motion.div className={styles.project} variants={itemVariants}>
             <Link href="/projects/dueto">
-              <div id="dueto">
+              <div id="dueto" className={styles.projectContainer}>
                 <Image publicId="Jubal_battisti/dueto/DUETO_Malika-133-Edit_by_Jubal_Battisti_mygtiu.jpg" />
                 <h3 className={styles.projectTitle}>DUETO</h3>
               </div>
