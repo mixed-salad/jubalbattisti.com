6ea02b93f358924ee8d882e754a502f8e0aab8f4
Harumi Terayama <hterayama@gmail.com>
2021-08-01 22:41:06 +0200

    P4 and Dueto page: Carousel image was not rendering inside of map() so I had to hard code the index 0

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,10 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { useEffect, useState } from 'react';
-import { getList } from './../api/cloudinary';
-import SliderArrow from './SliderArrows';
-import styles from './../styles/p4.module.scss';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useState } from "react";
+import { getList } from "./../api/cloudinary";
+import SliderArrow from "./SliderArrows";
+import styles from "./../styles/p4.module.scss";
 
 //const viewportWidth = window.innerWidth;
 
@@ -29,7 +29,7 @@ const Carousel = ({ category }) => {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    className: styles.slides
+    className: styles.slides,
   };
 
   return (
@@ -38,21 +38,34 @@ const Carousel = ({ category }) => {
       nextArrow={<SliderArrow type="next" />}
       prevArrow={<SliderArrow type="prev" />}
     >
+      <div className="wrapper">
+        <img
+          className={styles.sliderImg}
+          src={
+            category === "p4"
+              ? "https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/Jubal_battisti/p4/190409_P_4_Coming_Society_by_Jubal_Battisti_114-Edit_copy_hzeued.jpg"
+              : "https://res.cloudinary.com/jubalbattisti/image/upload/v1623597376/Jubal_battisti/dueto/DUETO_Robin-4-Edit_by_Jubal_Battisti_oud8vg.jpg"
+          }
+          alt={category === "p4" ? "Kate Strong" : "Robin"}
+        />
+      </div>
       {!!photoList &&
         photoList.map((image, index) => {
-          return (
-            <div className="wrapper" key={index}>
-              <img
-                className={styles.sliderImg}
-                src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${image.public_id}`}
-                alt={
-                  image.context
-                    ? image.context.custom.caption
-                    : 'Jubal Battisti Photography'
-                }
-              />
-            </div>
-          );
+          if (index !== 0) {
+            return (
+              <div className="wrapper" key={index}>
+                <img
+                  className={styles.sliderImg}
+                  src={`https://res.cloudinary.com/jubalbattisti/image/upload/v1619815218/${image.public_id}.${image.format}`}
+                  alt={
+                    image.context
+                      ? image.context.custom.caption
+                      : "Jubal Battisti Photography"
+                  }
+                />
+              </div>
+            );
+          }
         })}
     </Slider>
   );
